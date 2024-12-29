16012076165f7d1856d5bd0e8eda8818062e4cea
Taras B. <44473758+barmalini18@users.noreply.github.com>
2024-12-29 17:42:04 +0100

    update

--- projects/move_random_files.py
+++ projects/move_random_files.py
@@ -7,21 +7,24 @@ import sys
 import shutil as sh
 import random
 
+desired:int = 0
+if len(sys.argv) > 1:
+    desired:int = int(sys.argv[1])
+else:
+    print("Nothing to do, bye")
+    sys.exit()
+
 # Make a list of jpegs
 files = []
 files += [f for f in os.listdir() if f.endswith('.jpg')]
 
-desired:int = 0
-if len(sys.argv) > 1:
-    desired:int = int(sys.argv[1])
-
 # Check arguments
-if (0 <= desired >= len(files)):
-    # can't move more files then there is
-    print("Nothing to do, bye")
+if (desired >= len(files)):
+    # can't move more files than there is
+    print("Can't move more files than there is, bye")
     sys.exit()
 
-# Make a random list files to be moved
+# Make a random list of files to be moved
 to_move = random.sample(range(0, len(files)), desired)
 
 # Create new folder named by the number of files to be moved
@@ -33,3 +36,4 @@ if not os.path.exists(newpath):
 for f in to_move:
     print(files[f])
     sh.move (files[f], f'{newpath}\\{files[f]}')
+print("Done.")
