72e4a576347b28644688645dc86b5f38fd14246c
Taras B. <44473758+barmalini18@users.noreply.github.com>
2024-12-30 16:50:16 +0100

    refactor: clean up commented-out code in move_random_files.py

--- projects/move_random_files.py
+++ projects/move_random_files.py
@@ -2,41 +2,6 @@
 # # Example:  python move_random_files.py 21
 # # To move 21 jpegs to the folder named 21
 
-# import os
-# import sys
-# import shutil as sh
-# import random
-
-# desired:int = 0
-# if len(sys.argv) > 1:
-#     desired:int = int(sys.argv[1])
-# else:
-#     print("Nothing to do, bye")
-#     sys.exit()
-
-# # Make a list of jpegs
-# files = []
-# files += [f for f in os.listdir() if f.endswith('.jpg')]
-
-# # Check arguments
-# if (desired >= len(files)):
-#     print("Can't move more files than there is, bye")
-#     sys.exit()
-
-# # Make a list of random files to be moved
-# to_move = random.sample(range(0, len(files)), desired)
-
-# # Create new folder named by the number of files to be moved
-# newpath = str(desired)
-# if not os.path.exists(newpath):
-#     os.makedirs(newpath)
-
-# # Now move random files to the folder
-# for f in to_move:
-#     print(files[f])
-#     sh.move (files[f], f'{newpath}\\{files[f]}')
-# print("Done.")
-
 import os
 import sys
 import shutil as sh
@@ -53,7 +18,6 @@ def main():
     parser = argparse.ArgumentParser(description='Move a specified number of random JPEG files to a new folder.')
     parser.add_argument('num_files', type=int, help='Number of files to move')
     args = parser.parse_args()
-
     desired = args.num_files
 
     # Make a list of jpegs
